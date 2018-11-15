e0496473602a17ab1c6502b73525590ead5d3270
Santiago <santylore4@gmail.com>
2018-11-15 16:09:34 -0300

    some JSX code

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,13 +6,18 @@ import ReactDOM from 'react-dom'
 // Create a React components
 
 const App = () => {
+  const buttonText = { text: 'Click Me' }
+  const labelText = 'Enter Name:'
+
   return (
     <div>
-      <label for="name">
-        Enter name:
+      <label className="label" htmlFor="name">
+        {labelText}
       </label>
       <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText.text}
+      </button>
     </div>
   )
 }
