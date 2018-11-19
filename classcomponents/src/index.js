d61d3060a4e63501e437feab740979369c706b8c
Santiago <santylore4@gmail.com>
2018-11-18 22:41:42 -0300

    LifeCycle finished and simple app witch takes your location and shows something depending on your season

--- classcomponents/src/index.js
+++ classcomponents/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 class App extends React.Component {
 	constructor (props) {
@@ -8,6 +10,9 @@ class App extends React.Component {
 			lat: null,
 			errorMessage: ''
 		}
+	}
+
+	componentDidMount () {
 		window.navigator.geolocation.getCurrentPosition(
 			position => {this.setState({ lat: position.coords.latitude })},
 			err => {this.setState({ errorMessage: err.message })}
@@ -20,10 +25,10 @@ class App extends React.Component {
 		}
 		
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div>Latitude: {this.state.lat}</div>	
+			return <SeasonDisplay lat={this.state.lat} />	
 		}
 
-		return <div>Loading...</div>
+		return <Spinner message='Please accept location request'/>
 			
 	}
 }
